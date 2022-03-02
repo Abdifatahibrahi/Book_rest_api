5247185768f28283fc4def17523b4093b59cc71e
Abdifatah <alsidiq099@gmail.com>
2022-03-02 18:55:12 +0300

    Changed the serializers to Model serializers

--- book_api/serializer.py
+++ book_api/serializer.py
@@ -1,21 +1,27 @@
 from rest_framework import serializers
 from book_api.models import Book
-class BookSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField()
-    n_o_pages = serializers.IntegerField()
-    publish_date = serializers.DateField()
-    quantity = serializers.IntegerField()
+# class BookSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField()
+#     n_o_pages = serializers.IntegerField()
+#     publish_date = serializers.DateField()
+#     quantity = serializers.IntegerField()
 
 
-    def create(self, data):
-        return Book.objects.create(**data)
+#     def create(self, data):
+#         return Book.objects.create(**data)
 
-    def update(self, instance, data):
-        instance.title = data.get('title', instance.title)
-        instance.n_o_pages = data.get('n_o_pages', instance.n_o_pages)
-        instance.publish_date = data.get('publish_date', instance.publish_date)
-        instance.quantity = data.get('quantity', instance.quantity)
+#     def update(self, instance, data):
+#         instance.title = data.get('title', instance.title)
+#         instance.n_o_pages = data.get('n_o_pages', instance.n_o_pages)
+#         instance.publish_date = data.get('publish_date', instance.publish_date)
+#         instance.quantity = data.get('quantity', instance.quantity)
         
-        instance.save()
-        return instance
+#         instance.save()
+#         return instance
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = "__all__"
--- book_api/urls.py
+++ book_api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.book_create, name='create-book'),
-    path('list/', views.book_list, name='book-list'),
-    path('list/<int:pk>', views.book,)
+    path('', views.BookCreate.as_view(), name='create-book'),
+    path('list/', views.BookList.as_view(), name='book-list'),
+    path('list/<int:pk>', views.Book_detail.as_view())
 ]
